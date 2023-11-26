f8184a1a7125221e15956954cb7c3422aa7e7325
Bilal Daimoussi <bilal.daimoussi@ingrammicro.com>
2023-11-26 17:29:36 +0100

    CRUD-11: Enhance delete operation.

--- src/main/java/com/foundation/crud/repository/jpa/CustomerRepository.java
+++ src/main/java/com/foundation/crud/repository/jpa/CustomerRepository.java
@@ -11,4 +11,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CustomerRepository extends JpaRepository<Customer, Integer> {
 
     boolean existsCustomerByEmail(String email);
+    boolean existsCustomerById(Integer customerId);
 }
